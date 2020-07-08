c37b1c2485fa188e1de6c86bbfbba85d8af35a2b
Wesley <wesleyjcr@gmail.com>
2020-07-07 21:41:51 -0300

    😉 Removendo itens de uma lista

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -19,13 +19,22 @@ class TechList extends Component {
 
     this.setState({techs: [...this.state.techs, this.state.newTech]})
   }
+
+  renderDelete = (tech) => {
+    this.setState({techs: this.state.techs.filter(t => t !== tech)})
+  }
+
   
   render() {
     return( 
       <>
         <form onSubmit={this.renderSubmit}>
           <ul>
-            { this.state.techs.map(tech => <li key={ tech }>{ tech }</li>) }
+            { this.state.techs.map(tech => 
+              <li key={ tech }>
+                { tech }
+                <button type="button" onClick={() => this.renderDelete(tech)}>Remover</button>
+              </li>) }
           </ul>
           <input type="text" onChange={ this.renderInput } value={ this.state.newTech }/>
           <button type="submit">Enviado</button>
